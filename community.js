1d4ee8c6021b217e01152c3ed13eb8dd6132e0d7
tienduy0405 <53854899+tienduy0405@users.noreply.github.com>
2022-06-09 19:14:00 +0700

    Add files via upload

--- community.js
+++ community.js
@@ -1,6 +1,7 @@
 function displayData() {
     var name = document.getElementById("name");
     var content = document.getElementById("content");
+    window.location.href="community.html";
     document.getElementById("namePost").innerHTML= name;
     document.getElementById("contentPost").innerHTML = content;
 }
